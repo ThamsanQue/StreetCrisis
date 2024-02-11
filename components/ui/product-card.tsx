91ba0534e691a01f279201ab7e47716d0bd322b8
thamsanqaj <thamsanqa.dev@gmail.com>
2024-02-11 22:43:44 +0200

    used links instead of useRouter

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,9 +1,10 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 
 import { Product } from "@/types";
 import IconButton from "./icon-button";
-import { Expand, Link, ShoppingCart } from "lucide-react";
+import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
 import { MouseEventHandler } from "react";
 import usePreviewModal from "@/hooks/use-preview";
@@ -30,37 +31,37 @@ const ProductCard: React.FC<ProductCard> = ({ product }) => {
   };
 
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4 shadow-lg">
-      <div className="aspect-square rounded-xl bg-gray-100 relative">
-        <Link href={`/home/product/${product?.id}`} className="w-full h-full">
+    <Link href={`/home/product/${product?.id}`}>
+      <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4 shadow-lg">
+        <div className="aspect-square rounded-xl bg-gray-100 relative">
           <Image
             className="aspect-square object-scale-down rounded-md"
             src={product?.image?.[0]?.url}
             alt="Product Image"
             fill
           />
-        </Link>
-        <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
-          <div className="flex gap-x-6 justify-center">
-            <IconButton
-              onClick={onPreview}
-              icon={<Expand size={20} className="text-gray-600" />}
-            />
-            <IconButton
-              onClick={onAddToCart}
-              icon={<ShoppingCart size={20} className="text-gray-600" />}
-            />
+          <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
+            <div className="flex gap-x-6 justify-center">
+              <IconButton
+                onClick={onPreview}
+                icon={<Expand size={20} className="text-gray-600" />}
+              />
+              <IconButton
+                onClick={onAddToCart}
+                icon={<ShoppingCart size={20} className="text-gray-600" />}
+              />
+            </div>
+          </div>
+        </div>
+        <div>
+          <p className="font-semibold text-lg">{product.name}</p>
+          <p className="text-sm text-gray-500">{product.collection?.name}</p>
+          <div className="flex  items-center justify-between">
+            <Currency value={product?.price} />
           </div>
         </div>
       </div>
-      <div>
-        <p className="font-semibold text-lg">{product.name}</p>
-        <p className="text-sm text-gray-500">{product.collection?.name}</p>
-        <div className="flex  items-center justify-between">
-          <Currency value={product?.price} />
-        </div>
-      </div>
-    </div>
+    </Link>
   );
 };
 
